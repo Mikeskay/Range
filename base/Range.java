2fb2d7f6b1cf492b632fd63b9de78298d3fd21c0
Michael Kolawole <47972946+Mikeskay@users.noreply.github.com>
2022-03-23 17:24:25 +0000

    Create Range.java

---
+++ base/Range.java
@@ -0,0 +1,27 @@
+package range.base;
+
+public class Range {
+
+  public Range(){
+
+  }
+
+  public void findRange(int range1, int range2){
+  
+    
+          if (range2 <= range1) {
+            System.out.println ("Error: Second Value is less than the first");
+    
+          }
+    
+          else {
+    
+            System.out.println (range1 + range2);
+    
+    
+            }
+    
+  }
+  
+
+}
